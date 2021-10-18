44adad8fd36c74528124ae37bcd34b91a1817844
JAWA <jawa-webdesign@outlook.com>
2021-10-18 18:03:16 +0200

    première version

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+//const cors = require("cors");
+
+const UserModel = require("./models/userModels");
+const userRoute = require("./routes/userRoute");
+
+//* Connexion à MongoDB avec mongoose :
+
+mongoose
+  .connect(
+    "mongodb+srv://julienARMAND:Bagus_database_access33@cluster0.qbney.mongodb.net/piiquante?retryWrites=true&w=majority",
+    //! Modification de la connexion avec un .env pour ne pas que les codes de connexion soit disponibles !!!//
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//* Utilisation d'express :
+
+const app = express();
+
+//* Autorisation CORS (version openclassrooms):
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+//* Autorisation CORS (version fromscratch):
+//app.use(cors());
+
+//* Connexion pour envoi sans body-parser :
+app.use(express.json());
+
+app.use("/api/auth", userRoute);
+
+//* Route Signup :
+
+// app.post("/api/auth/signup", (req, res) => {
+//   console.log(req.body);
+//   const user = new UserModel({
+//     ...req.body,
+//   });
+//   user
+//     .save()
+//     .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+//     .catch((err) => res.status(400).json("voici " + err));
+// });
+module.exports = app;
---
+++ models/userModels.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+//const { isEmail } = require("validator");
+//const { isStrongPassword } = require("validator");
+// const bcrypt = require("bcrypt");
+
+//* création du schéma Utilisateur : --------------------------------------//
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    //validate: [isEmail],
+    lowercase: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    //validate: [isStrongPassword],
+    maxlength: 1024,
+    minlength: 6,
+  },
+});
+
+//* email unique :
+
+userSchema.plugin(uniqueValidator);
+
+//* Fonction de hachage du mot de passe (salage) avant enregistrement dans la base de données -----//
+
+// userSchema.pre("save", async function (next) {
+//   const salt = await bcrypt.genSalt();
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
+
+// userSchema.statics.login = async function (email, password) {
+//   const user = await this.findOne({ email });
+//   if (user) {
+//     const auth = await bcrypt.compare(password, user.password);
+//     if (auth) {
+//       //console.log(user);
+//       return user;
+//     }
+//     throw Error("Mot de passe incorrect !");
+//   }
+//   throw Error("Email non valide !");
+// };
+
+//* Export du schéma en tant que modèle :--------------------------//
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
