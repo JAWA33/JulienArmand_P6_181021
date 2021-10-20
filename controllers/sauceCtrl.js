b02d3b22e3aa3f8356827c4e46d28acb9bf3ed36
JAWA <jawa-webdesign@outlook.com>
2021-10-20 14:16:48 +0200

    Modification route Put pour suppression de l'ancienne image si besoin

--- controllers/sauceCtrl.js
+++ controllers/sauceCtrl.js
@@ -38,21 +38,28 @@ exports.createSauce = (req, res, next) => {
 //* Modifier une sauce existante :
 
 exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file
-    ? {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
-      }
-    : { ...req.body };
-  SauceModel.updateOne(
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
-  )
-    .then(() => res.status(200).json({ message: "Sauce mise à jour !" }))
-    .catch((error) => res.status(400).json({ error }));
-  //next();
+  //* test suppression image existante
+  SauceModel.findOne({ _id: req.params.id }).then((sauce) => {
+    const filename = sauce.imageUrl.split("/images/")[1];
+    fs.unlink(`images/${filename}`, () => {
+      const sauceObject = req.file
+        ? {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`,
+          }
+        : { ...req.body };
+      SauceModel.updateOne(
+        { _id: req.params.id },
+        { ...sauceObject, _id: req.params.id }
+      )
+        .then(() => res.status(200).json({ message: "Sauce mise à jour !" }))
+        .catch((error) => res.status(400).json({ error }));
+    }).catch((error) => res.status(404).json({ error }));
+
+    //next();
+  });
 };
 
 //* Supprimer une sauce existante :
