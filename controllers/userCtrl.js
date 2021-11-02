2cb387fc5f5a8f68f2f97a1e4e7ee922ecc885d1
JAWA <jawa-webdesign@outlook.com>
2021-11-02 09:55:38 +0100

    Ajout Regex sur email et password

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -4,21 +4,65 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new UserModel({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur Créé !" }))
-        .catch((err) => res.status(400).json({ err }));
-    })
-    .catch((err) => res.status(500).json({ err }));
-  //next();
+  if (validEmail(req.body.email) && validPassword(req.body.password)) {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = new UserModel({
+          email: req.body.email,
+          password: hash,
+        });
+        user
+          .save()
+          .then(() => res.status(201).json({ message: "Utilisateur Créé !" }))
+          .catch((err) => res.status(400).json({ err }));
+      })
+      .catch((err) => res.status(500).json({ err }));
+    //next();
+  } else if (!validEmail(req.body.email)) {
+    return res.status(401).json({
+      message:
+        "E-mail non valide : doit contenir une adresse de type : monadresse@site.com ",
+    });
+  } else if (!validPassword(req.body.password)) {
+    return res.status(402).json({
+      message:
+        "Votre mot de passe doit contenir au minimum : un nombre, une lettre minuscule, une lettre majuscule et avoir entre 8 et 16 caractères",
+    });
+  }
 };
+
+//! REGEX : validation des données de la requête ----------------------------------
+//* Test Email :*****************/
+const validEmail = (checkMail) => {
+  let regexEmail = new RegExp(
+    "^[A-Za-z0-9.-_--]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,3}$",
+    "g"
+  );
+  let testEmail = regexEmail.test(checkMail);
+
+  if (testEmail) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//* Test Password:*****************/
+const validPassword = (checkPass) => {
+  let regexPass = new RegExp(
+    "^(?=.*d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^wds:])([^s]){8,16}$",
+    "g"
+  );
+  let testPass = regexPass.test(checkPass);
+
+  if (testPass) {
+    return true;
+  } else {
+    return false;
+  }
+};
+//! Fin REGEX : validation des données de la requête----------------------------------
 
 exports.login = (req, res, next) => {
   UserModel.findOne({ email: req.body.email })
